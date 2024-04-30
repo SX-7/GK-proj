446d37ada0fec69cfac1eee8dd4ad9a4eecf6be0
SX_7 <sn1.test.preria.2002@gmail.com>
2024-04-30 21:16:10 +0200

    Added info

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //make sure we grab something
         if (rb == null) { rb = GetComponent<Rigidbody>(); }
         if (cam == null) { cam = GetComponentInChildren<Camera>(); }
         if (col == null) { col = GetComponent<CapsuleCollider>(); }
@@ -27,6 +28,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //currently it's a very limited scenario, but if we are in an animation, we want to take control away from the player
         if (!climbing)
         {
             ProcessMovement();
@@ -44,6 +46,7 @@ public class PlayerController : MonoBehaviour
             }
             else
             {
+                // Check for possible vaulting
                 if (vaultTargets.Count > 0)
                 {
                     var positions = vaultTargets.Select(
@@ -60,13 +63,14 @@ public class PlayerController : MonoBehaviour
         }
         //cam.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
         //cam.transform.Rotate(cam.transform.right, Input.GetAxis("Mouse Y"));
-        
+
         if (Input.GetButton("Crouch"))
         {
             CrouchingMode(true);
         }
         else
         {
+            //it is possible to stop crouch when you can't physically uncrouch, due to no limited friction during crouch, we allow movement to avoid softlocks
             CrouchingMode(false);
             Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         }
@@ -82,7 +86,8 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            if(Physics.RaycastAll(
+            //check if we can uncrouch
+            if (Physics.RaycastAll(
                     transform.TransformPoint(col.center) + new Vector3(0, col.bounds.size.y / 3, 0),
                     Vector3.up,
                     col.bounds.size.y
@@ -93,13 +98,14 @@ public class PlayerController : MonoBehaviour
                 transform.localScale = new Vector3(1, 1, 1);
                 cam.transform.position = transform.position + new Vector3(0, 0.5f, 0);
             }
-            
+
         }
 
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //Track availible vault targets
         if (other.GetComponent<Climbable>())
         {
             vaultTargets.Add(other);
@@ -121,6 +127,7 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 GetClimbableVaultTarget(Collider other)
     {
+        //finds a destination point above nearest climb plane
         var contact_point = other.transform.InverseTransformPoint(other.ClosestPointOnBounds(transform.position));
         contact_point.y = other.GetComponent<Climbable>().climbLevels.Where(x => x / other.transform.localScale.y >= contact_point.y).Min() / other.transform.localScale.y;
         var destination = other.transform.TransformPoint(contact_point);
@@ -129,6 +136,7 @@ public class PlayerController : MonoBehaviour
 
     private bool OnWalkable()
     {
+        //test if object with walkable is *right* below us (we're standing on it)
         return Physics.RaycastAll(
                     transform.TransformPoint(col.center) - new Vector3(0, col.bounds.size.y / 3, 0),
                     Vector3.down,
@@ -145,6 +153,7 @@ public class PlayerController : MonoBehaviour
 
     private void RotateCamera(float x_delta, float y_delta)
     {
+        //helper function
         viewYAngle -= y_delta;
         viewYAngle = Mathf.Clamp(viewYAngle, -80, 80);
         viewXAngle += x_delta;
@@ -157,30 +166,31 @@ public class PlayerController : MonoBehaviour
         // calculate path (up then forward)
         var midpoint = new Vector3(transform.position.x, destination.y, transform.position.z);
         // start coroutine with path 
-        StartCoroutine(ClimbCR(midpoint,destination));
+        StartCoroutine(ClimbCR(midpoint, destination));
     }
 
     private IEnumerator ClimbCR(Vector3 midpoint, Vector3 destination)
     {
         climbing = true;
         rb.velocity = Vector3.zero;
+        //we attempt to finish the animation in one constant motion. might be changed later
         var anim_time = climbingTime;
         var start = transform.position;
         var to_mid_len = Vector3.Distance(start, midpoint);
         var to_end_len = Vector3.Distance(midpoint, destination);
         var to_mid_time = anim_time * to_mid_len / (to_mid_len + to_end_len);
         var to_end_time = anim_time * to_end_len / (to_mid_len + to_end_len);
-        for (float i = 0; i < to_mid_time; i+=Time.deltaTime)
+        for (float i = 0; i < to_mid_time; i += Time.deltaTime)
         {
-            transform.position = Vector3.Lerp(start, midpoint, i/to_mid_time);
+            transform.position = Vector3.Lerp(start, midpoint, i / to_mid_time);
             yield return null;
         }
         for (float i = 0; i < to_end_time; i += Time.deltaTime)
         {
-            transform.position = Vector3.Lerp(midpoint, destination, i/to_end_time);
+            transform.position = Vector3.Lerp(midpoint, destination, i / to_end_time);
             yield return null;
         }
         climbing = false;
-        rb.velocity = (destination-midpoint).normalized*1f;
+        rb.velocity = (destination - midpoint).normalized * 10f;
     }
 }
--- Assets/Scripts/Walkable.cs
+++ Assets/Scripts/Walkable.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Walkable : MonoBehaviour
-{
+{ //empty, used for marking platforms which can be jumped off of
 }
