f63e9eef0b020f15a4aaa3ca6775027d960bf6af
SX_7 <sn1.test.preria.2002@gmail.com>
2024-05-01 15:09:47 +0200

    Added hand floating

---
+++ Assets/Scripts/HandFloater.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandFloater : MonoBehaviour
+{
+    [SerializeField] PlayerController controller;
+    private Rigidbody playerRb;
+    private Vector3 initPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = transform.localPosition;
+        controller = FindAnyObjectByType<PlayerController>();
+        playerRb = controller.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var destination = initPos + new Vector3(
+                -controller.transform.InverseTransformDirection(playerRb.velocity).x / 500,
+                -controller.transform.InverseTransformDirection(playerRb.velocity).y / 500,
+                -controller.transform.InverseTransformDirection(playerRb.velocity).z / 500
+            );
+        transform.localPosition = Vector3.Lerp(transform.localPosition, destination, 0.02f);
+        //Debug.Log(0.1f - (timer / (timerFreq * 10)));
+    }
+}
