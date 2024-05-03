b6212be034e0d821b0474b11b0d099753c14e2b7
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2024-05-03 18:49:23 +0200

    RotateObject script modification

--- Assets/Scripts/PlatformMovement/RotateObject.cs
+++ Assets/Scripts/PlatformMovement/RotateObject.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
-    public float rotationSpeed = 90f;  // Rotation speed in degrees per second
+    public Vector3 rotationAxis = Vector3.up;  // Axis around which the object will rotate
+    public float rotationSpeed = 90.0f;  // Speed of rotation in degrees per second
+
+    void Start()
+    {
+        // Generate a random initial rotation between 0 and 360 degrees
+        float initialRotation = Random.Range(0f, 360f);
+
+        // Apply the initial rotation offset
+        transform.Rotate(rotationAxis, initialRotation);
+    }
 
     void Update()
     {
-        // Rotate the object around its local Y-axis at the specified rotation speed
-        transform.RotateAround(transform.position, transform.up, rotationSpeed * Time.deltaTime);
+        // Rotate around the specified axis at 'rotationSpeed' degrees per second.
+        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
     }
 }
